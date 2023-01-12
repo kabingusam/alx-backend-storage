88ec3263ca42096df9a1da438fac7fc4c31bc752
kabingusam <kabingusammy@gmail.com>
2023-01-12 15:50:01 +0300

    update on task and file structure

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,13 +1,14 @@
 -- This script creates a trigger that decreases the quantity of an item after adding a new order
 -- It uses delimiters to separate the trigger logic from the rest of the script
 
+DROP TRIGGER IF EXISTS decrease_quantity
 DELIMITER $$
 CREATE TRIGGER decrease_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
     UPDATE items
-    SET quantity = quantity - NEW.quantity
-    WHERE id = NEW.item_id;
+        SET quantity = quantity - NEW.quantity
+        WHERE id = NEW.item_id;
 END$$
 DELIMITER ;
