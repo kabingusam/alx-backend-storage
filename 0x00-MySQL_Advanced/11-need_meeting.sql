c69133d6fde5a2aff4d6ddec956bf2d60cfdd9e3
kabingusam <kabingusammy@gmail.com>
2023-01-13 16:10:23 +0300

    update on date

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -6,5 +6,5 @@ CREATE VIEW need_meeting AS
         WHERE score < 80 AND 
         (
             last_meeting IS NULL
-            OR last_meeting < SUBDATE(NOW(), INTERVAL 1 MONTH)
+            OR last_meeting < SUBDATE(CURRENT_DATE(), INTERVAL 1 MONTH)
         );
