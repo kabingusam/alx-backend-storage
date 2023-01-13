4128da3f24ce428450d8f229373e2d324d386002
kabingusam <kabingusammy@gmail.com>
2023-01-13 12:40:51 +0300

    update on task completion

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,15 @@
+-- Trigger to update email
+
+DROP TRIGGER IF EXISTS verfiy_email;
+DELIMITER $$
+CREATE TRIGGER verfiy_email
+AFTER INSERT ON users
+FOR EACH ROW
+BEGIN
+    IF OLD.email != NEW.email THEN
+        SET NEW.valid_email = 0;
+    ELSE
+        SET NEW.valid_email = NEW.valid_email;
+    END IF;
+END $$
+DELIMITER ;
