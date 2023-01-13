67c7debbc4953b05d7c328ef84c8025b5540ab07
kabingusam <kabingusammy@gmail.com>
2023-01-13 16:05:23 +0300

    inclusion of IS NULL

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -2,7 +2,9 @@
 DROP VIEW IF EXISTS need_meeting
 CREATE VIEW need_meeting AS
     SELECT name 
-    FROM students 
-    WHERE score < 80 AND (
-        last_meeting  = 0 OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH)
-    );
+        FROM students 
+        WHERE score < 80 AND 
+        (
+            last_meeting IS NULL
+            OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH)
+        );
