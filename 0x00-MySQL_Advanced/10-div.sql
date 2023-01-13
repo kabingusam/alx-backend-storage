75bac5a966087447efe47b89ffaa41a4fbdfe871
kabingusam <kabingusammy@gmail.com>
2023-01-13 15:22:10 +0300

    syntax and delimiter update

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,11 +1,15 @@
 -- a function SafeDiv that returns the divison of two numbers
 
+DROP FUNCTION IF EXISTS SafeDiv;
+DELIMITER $$
+
 CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURN INT
+RETURN FLOAT
 BEGIN 
     IF b = 0 THEN
         RETURN 0;
     ELSE
         RETURN a / b;
     END IF;
-END;
+END $$
+DELIMITER ;
